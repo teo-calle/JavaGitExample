bddf3ba8ac914c0331482122ce6481963e18345e
mac <mac@10.211.55.3>
2016-02-24 11:40:50 -0500

    Version 1.0 Datos version de prueba conexion de GitHup

---
+++ src/javagitexample/JavaGitExample.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javagitexample;
+
+/**
+ *
+ * @author mac
+ */
+public class JavaGitExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Control de versiones con GitHub");
+        // TODO code application logic here
+    }
+    
+}
